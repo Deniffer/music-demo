5862c676269b7b6914be0afbbe032ac635b7a2d4
Deniffer <lovewyqforever@gmail.com>
2024-07-16 19:30:41 +0800

    feat: add some mock data

--- models/song.tsx
+++ models/song.tsx
@@ -2,6 +2,277 @@ import { Song } from "@/types/song";
 
 const songs: Song[] = [
   {
+    uuid: "5af155c2-2dbb-40b2-940f-33dd1d39681b",
+    video_url: "https://cdn1.suno.ai/5af155c2-2dbb-40b2-940f-33dd1d39681b.mp4",
+    audio_url: "https://cdn1.suno.ai/5af155c2-2dbb-40b2-940f-33dd1d39681b.mp3",
+    image_url:
+      "https://cdn1.suno.ai/image_0d0cd4c7-0db2-40b6-9858-3be265a26d2e.png",
+    image_large_url:
+      "https://cdn1.suno.ai/image_large_0d0cd4c7-0db2-40b6-9858-3be265a26d2e.png",
+    tags: "Drum and bass",
+    lyrics:
+      "Sinking under\n\nHold me\n\nTouch me\n\nSave me\n\nFalling faster\n\nCan you hear me?\n\nSee me\n\nBecome me\n\nSinking under\n\nHold me\n\nSave me\n\nFalling faster\n\nCan you hear me?\n\n[Lengthy Outro]",
+    duration: 159.99997916666666,
+    title: "First Law",
+    play_count: 15,
+    upvote_count: 1,
+    created_at: "2024-04-08 08:58:24.864+00",
+    status: "complete",
+    is_public: true,
+    is_trending: false,
+    type: "concat",
+  },
+  {
+    uuid: "2a396543-07a5-4d82-bcf0-6ed8f5cbf5a1",
+    video_url: "https://cdn1.suno.ai/2a396543-07a5-4d82-bcf0-6ed8f5cbf5a1.mp4",
+    audio_url: "https://cdn1.suno.ai/2a396543-07a5-4d82-bcf0-6ed8f5cbf5a1.mp3",
+    image_url:
+      "https://cdn1.suno.ai/image_b4e3f6f4-c576-4103-9f98-58be58ea785d.png",
+    image_large_url:
+      "https://cdn1.suno.ai/image_large_b4e3f6f4-c576-4103-9f98-58be58ea785d.png",
+    tags: "Symphonique, orchestral avec chœurs masculins massifs, épique, guerrier et martial, rythmé et percutant, grandiose",
+    lyrics:
+      "(Refrain)\nConan, bellator in armis,\nVictor in pugna, fortis et ferus.\nIn vastis terris, pericula superans,\nGladio suo, hostes opprimens.\n\n(Strophe 1)\nIn umbra montium, in deserto vasto,\nConan vagatur, solus et vasto.\nSagittas hostium, et tela acuta,\nEvasit semper, fortis et impavida.\n\n(Refrain)\nConan, bellator in armis,\nVictor in pugna, fortis et ferus.\nIn vastis terris, pericula superans,\nGladio suo, hostes opprimens.",
+    duration: 378.87997916666666,
+    title: "Conan Bellator: Triumphus Martialis",
+    play_count: 0,
+    upvote_count: 1,
+    created_at: "2024-04-08 09:07:05.681+00",
+    status: "complete",
+    is_public: true,
+    is_trending: false,
+    type: "concat",
+  },
+  {
+    uuid: "0fee70fe-d40d-490a-a5f1-1f3230a46039",
+    video_url: "https://cdn1.suno.ai/0fee70fe-d40d-490a-a5f1-1f3230a46039.mp4",
+    audio_url: "https://cdn1.suno.ai/0fee70fe-d40d-490a-a5f1-1f3230a46039.mp3",
+    image_url:
+      "https://cdn1.suno.ai/image_31a4f8b8-d817-4168-a19a-a322c361d0f9.png",
+    image_large_url:
+      "https://cdn1.suno.ai/image_large_31a4f8b8-d817-4168-a19a-a322c361d0f9.png",
+    tags: "hawaiian dance style,male voice",
+    lyrics:
+      "[instrumental intro]\n\n(Verse 1)\n趕羚洋 草枝擺\n羚洋最愛眾人趕\n\n八月中秋 山林涼\n風吹大地 草枝擺\n趕羚洋草枝擺\n羚洋最愛眾人趕\n眾人趕阿 (趕羚洋)\n趕羚洋\n\n(Chorus)\n甘霖老母 趕羚洋 (趕羚洋)\n草枝擺阿 趕羚洋 (趕羚洋)\n趕羚洋阿 草枝擺 (草枝擺)",
+    duration: 89.99997916666666,
+    title: "趕羚羊",
+    play_count: 0,
+    upvote_count: 0,
+    created_at: "2024-04-08 09:03:40.523+00",
+    status: "complete",
+    is_public: true,
+    is_trending: false,
+    type: "concat",
+  },
+  {
+    uuid: "eb48f05c-2b6c-4a79-901b-d154ea01a42d",
+    video_url: "https://cdn1.suno.ai/eb48f05c-2b6c-4a79-901b-d154ea01a42d.mp4",
+    audio_url: "https://cdn1.suno.ai/eb48f05c-2b6c-4a79-901b-d154ea01a42d.mp3",
+    image_url:
+      "https://cdn1.suno.ai/image_6739b381-b589-44a3-8a7b-63f4a17cc88b.png",
+    image_large_url:
+      "https://cdn1.suno.ai/image_large_6739b381-b589-44a3-8a7b-63f4a17cc88b.png",
+    tags: "orchestro drum and bass 150 bpm",
+    lyrics:
+      "[electro guitar]\n[chorus]\nHo hey tam tararam\nHo hey tam tararam\nHo hey tam tararam\n[drop]\n[chorus]\nHo hey tam tararam\nHo hey tam tararam\nHo hey tam tararam\n\n\n[chorus]\nHo hey tam tararam\nhu hey tu tururum\nHo hey tam tararam\nhu hey tu tururum\n\n\n\n[chorus]\nHo hey bum bururum\nhu hey tu tururum\nHo hey bum bururum\nhu hey tu tururum\n[end]",
+    duration: 226.51997916666667,
+    title: "COre",
+    play_count: 7,
+    upvote_count: 1,
+    created_at: "2024-04-10 20:08:35.24+00",
+    status: "complete",
+    is_public: true,
+    is_trending: false,
+    type: "concat",
+  },
+  {
+    uuid: "382d0061-4994-4c81-b488-3f1cd5945816",
+    video_url: "https://cdn1.suno.ai/382d0061-4994-4c81-b488-3f1cd5945816.mp4",
+    audio_url: "https://cdn1.suno.ai/382d0061-4994-4c81-b488-3f1cd5945816.mp3",
+    image_url:
+      "https://cdn1.suno.ai/image_da44d07b-fdc1-43c1-aa79-e2428b6c87de.png",
+    image_large_url:
+      "https://cdn1.suno.ai/image_large_da44d07b-fdc1-43c1-aa79-e2428b6c87de.png",
+    tags: "dnb, chill house",
+    lyrics:
+      "[aggressive woman voice]\n\nInto the Abyss of My Mind\n\nVerse 1:\nIn the darkness I reside, fear creeping deep inside\nParanoia's haunting grip, shadows twisting through my mind\nRazor blades of doubt, cutting through my sanity\nIn this twisted landscape, I fight to keep my clarity\n\nChorus:\nInto the abyss of my mind, I descend\nLost in the chaos, my demons close in\nParanoia and depression, devouring my soul\nBut I'll fight for survival, to finally take control",
+    duration: 183.79997916666667,
+    title: "",
+    play_count: 7,
+    upvote_count: 1,
+    created_at: "2024-04-08 12:04:45.359+00",
+    status: "complete",
+    is_public: true,
+    is_trending: true,
+    type: "concat",
+  },
+  {
+    uuid: "6841377d-d7e3-4c27-adf2-f27f51d19158",
+    video_url: "https://cdn1.suno.ai/6841377d-d7e3-4c27-adf2-f27f51d19158.mp4",
+    audio_url: "https://cdn1.suno.ai/6841377d-d7e3-4c27-adf2-f27f51d19158.mp3",
+    image_url:
+      "https://cdn1.suno.ai/image_1e0c4628-c0ae-4e8b-a808-0873024eef60.png",
+    image_large_url:
+      "https://cdn1.suno.ai/image_large_1e0c4628-c0ae-4e8b-a808-0873024eef60.png",
+    tags: "fast pase electro swing",
+    lyrics:
+      "[Verse]\nWoke up this morning, same old routine\nClocking in and out, like a machine\nTired of the grind, feeling stuck in a haze\nBut I refuse to let my dreams go to waste\n\n[Verse 2]\nI picked up a guitar, started strummin' a tune\nFound my voice, realized I had something to prove\nI sang my heart out, let my spirit soar\nAnd now I'm watchin' all those doubters hit the floor (oh-oh-oh)\n\n[Chorus]\nSo watch me rise, I'm stepping out of the shadows (ooh-yeah)\nNo more hiding, no more pain to swallow\nI'm gonna make it, succeed against the odds\nFrom ordinary worker to a superstar, I'll rise above",
+    duration: 209.95997916666667,
+    title: "Breakin' Free",
+    play_count: 0,
+    upvote_count: 0,
+    created_at: "2024-04-08 12:03:18.859+00",
+    status: "complete",
+    is_public: true,
+    is_trending: true,
+    type: "concat",
+  },
+  {
+    uuid: "3b33e10b-00a3-42ef-bdd8-b564be91334d",
+    video_url: "https://cdn1.suno.ai/3b33e10b-00a3-42ef-bdd8-b564be91334d.mp4",
+    audio_url: "https://cdn1.suno.ai/3b33e10b-00a3-42ef-bdd8-b564be91334d.mp3",
+    image_url:
+      "https://cdn1.suno.ai/image_5c1f61b6-b68f-446b-a407-8605612af66d.png",
+    image_large_url:
+      "https://cdn1.suno.ai/image_large_5c1f61b6-b68f-446b-a407-8605612af66d.png",
+    tags: "progressive rock folk",
+    lyrics: "[Instrumental]",
+    duration: 263.99997916666666,
+    title: "The Journey of Time",
+    play_count: 75,
+    upvote_count: 2,
+    created_at: "2024-04-08 16:21:46.218+00",
+    status: "complete",
+    is_public: true,
+    is_trending: true,
+    type: "concat",
+  },
+  {
+    uuid: "a668f229-8b97-46d0-8d0e-647124141d8e",
+    video_url: "https://cdn1.suno.ai/a668f229-8b97-46d0-8d0e-647124141d8e.mp4",
+    audio_url: "https://cdn1.suno.ai/a668f229-8b97-46d0-8d0e-647124141d8e.mp3",
+    image_url:
+      "https://cdn1.suno.ai/image_08b09d61-f901-4978-806d-2284fc88ffb2.png",
+    image_large_url:
+      "https://cdn1.suno.ai/image_large_08b09d61-f901-4978-806d-2284fc88ffb2.png",
+    tags: "trap, ethereal, whispering female vocals",
+    lyrics: "",
+    duration: 174.55997916666666,
+    title: "Equinox",
+    play_count: 9,
+    upvote_count: 0,
+    created_at: "2024-04-08 23:41:28.114+00",
+    status: "complete",
+    is_public: true,
+    is_trending: false,
+    type: "concat",
+  },
+  {
+    uuid: "ee467d00-5813-4a74-9792-c9ae4a09d344",
+    video_url: "https://cdn1.suno.ai/ee467d00-5813-4a74-9792-c9ae4a09d344.mp4",
+    audio_url: "https://cdn1.suno.ai/ee467d00-5813-4a74-9792-c9ae4a09d344.mp3",
+    image_url:
+      "https://cdn1.suno.ai/image_31aab0ea-0e9a-4eca-b749-e4646f9df348.png",
+    image_large_url:
+      "https://cdn1.suno.ai/image_large_31aab0ea-0e9a-4eca-b749-e4646f9df348.png",
+    tags: "Cat",
+    lyrics:
+      "Cat Cat Cat Cat Cat Cat Cat Cat Cat Cat Cat\nCat Cat Cat Cat Cat Cat Cat Cat Cat Cat Cat\nCat Cat Cat Cat Cat Cat Cat Cat Cat Cat Cat\nCat Cat Cat Cat Cat Cat Cat Cat Cat Cat Cat\nCat Cat Cat Cat Cat Cat Cat Cat Cat Cat Cat\n\nCat Cat Cat Cat Cat Cat Cat Cat Cat Cat Cat\nCat Cat Cat Cat Cat Cat Cat Cat Cat Cat Cat\nCat Cat Cat Cat Cat Cat Cat Cat Cat Cat Cat",
+    duration: 167.83997916666667,
+    title: "Cat",
+    play_count: 15017,
+    upvote_count: 568,
+    created_at: "2024-04-06 22:28:13.769+00",
+    status: "complete",
+    is_public: true,
+    is_trending: true,
+    type: "concat",
+  },
+  {
+    uuid: "03a77bb2-4a04-4829-b98e-6b97bcae32e7",
+    video_url: "https://cdn1.suno.ai/03a77bb2-4a04-4829-b98e-6b97bcae32e7.mp4",
+    audio_url: "https://cdn1.suno.ai/03a77bb2-4a04-4829-b98e-6b97bcae32e7.mp3",
+    image_url:
+      "https://cdn1.suno.ai/image_e2e8484d-2954-436c-a677-fe63aac6958e.png",
+    image_large_url:
+      "https://cdn1.suno.ai/image_large_e2e8484d-2954-436c-a677-fe63aac6958e.png",
+    tags: "dubstep, trap, sinth pop, emo",
+    lyrics:
+      "ah ah ah\n\n[Chorus]\nAbito la tempesta...\nHo lasciato che il vostro dolore mi scolpisse; \n\n[Verse]\nsanguinando ho guadagnato un volto: ogni sbreccìata è stata rabbia, violenza, grido, pianto, a volte un fiore... \n\n[Verse2]\ne per questo vivo. I ricordi franano sull'anima, sferzata dalla tempesta, ed io la abito perchè essa ha un cuore...",
+    duration: 158.95997916666667,
+    title: "Abito la Tempesta (Testo di U. Panipucci)",
+    play_count: 1,
+    upvote_count: 0,
+    created_at: "2024-04-08 12:14:26.232+00",
+    status: "complete",
+    is_public: true,
+    is_trending: false,
+    type: "concat",
+  },
+  {
+    uuid: "dddf2419-310f-43b9-93b2-e2ae7a15026a",
+    video_url: "https://cdn1.suno.ai/dddf2419-310f-43b9-93b2-e2ae7a15026a.mp4",
+    audio_url: "https://cdn1.suno.ai/dddf2419-310f-43b9-93b2-e2ae7a15026a.mp3",
+    image_url:
+      "https://cdn1.suno.ai/image_952602a0-d1c8-4cf3-8ebe-b087982f5733.png",
+    image_large_url:
+      "https://cdn1.suno.ai/image_large_952602a0-d1c8-4cf3-8ebe-b087982f5733.png",
+    tags: "drum & bass female edm",
+    lyrics:
+      "[Verse]\nI hear your lies, your treacherous disguise\nYou think you're sly, but I see through those eyes\nYour empty words, they burn like acid rain\nBut I won't let your poison invade my veins\n\n[Verse 2]\nYou thought I'd crumble, but I'm stronger than you know\nI'll rise from the ashes, my spirit won't be controlled\nI'll dance through the fire, ignite the fury within\nNo longer a victim, I'll be the one who wins\n\n[Chorus]\nFeel the firestorm, burning down the walls\nRaging like a tempest, I won't be your downfall\nWith every beat, my power grows\nIn this EDM revolution, watch me as I explode",
+    duration: 194.95997916666667,
+    title: "Firestorm",
+    play_count: 0,
+    upvote_count: 0,
+    created_at: "2024-04-08 09:01:36.976+00",
+    status: "complete",
+    is_public: true,
+    is_trending: false,
+    type: "concat",
+  },
+  {
+    uuid: "fc4056ad-2445-404c-a8fc-96889317a0d8",
+    video_url: "https://cdn1.suno.ai/fc4056ad-2445-404c-a8fc-96889317a0d8.mp4",
+    audio_url: "https://cdn1.suno.ai/fc4056ad-2445-404c-a8fc-96889317a0d8.mp3",
+    image_url:
+      "https://cdn1.suno.ai/image_eabf2179-521a-40d4-b7bf-c00badbd3b4b.png",
+    image_large_url:
+      "https://cdn1.suno.ai/image_large_eabf2179-521a-40d4-b7bf-c00badbd3b4b.png",
+    tags: "Drum and bass",
+    lyrics:
+      "Sinking under\n\nHold me\n\nTouch me\n\nSave me\n\nFalling faster\n\nCan you hear me?\n\nSee me\n\nBecome me\n\nSinking under\n\nHold me\n\nSave me\n\nFalling faster\n\nCan you hear me?\n\n[Lengthy Outro]",
+    duration: 169.99997916666666,
+    title: "First Law",
+    play_count: 0,
+    upvote_count: 1,
+    created_at: "2024-04-08 08:58:32.057+00",
+    status: "complete",
+    is_public: true,
+    is_trending: false,
+    type: "concat",
+  },
+  {
+    uuid: "921ee78f-8aa1-49d4-a1f1-d965f935807b",
+    video_url: "https://cdn1.suno.ai/921ee78f-8aa1-49d4-a1f1-d965f935807b.mp4",
+    audio_url: "https://cdn1.suno.ai/921ee78f-8aa1-49d4-a1f1-d965f935807b.mp3",
+    image_url:
+      "https://cdn1.suno.ai/image_d1c5f803-46ab-475d-a8b9-39196b4d079b.png",
+    image_large_url:
+      "https://cdn1.suno.ai/image_large_d1c5f803-46ab-475d-a8b9-39196b4d079b.png",
+    tags: "introspective midtempo pop",
+    lyrics:
+      "[Verse]\nHey, it's been a while since you saw my face\nI haven't been doing so great, caught in a chase\nSo I took a little break, needed space to clear my mind\nBut now I'm back, ready to leave it all behind\n\n[Verse 2]\nA lot of people are saying some things about me\nBut they don't know the truth, just what they wanna see\nDoesn't matter if it's true or not, the rumors fly\nI'm stronger than ever, ready to touch the sky\n\n[Chorus]\nI'm a stranger in the mirror, a different side\nI know who I am, I won't let it hide\nI've learned to rise above, to be true to myself\nNo matter what they say, I'm breaking free from their spell",
+    duration: 123.99997916666666,
+    title: "Stranger in the Mirror",
+    play_count: 17,
+    upvote_count: 1,
+    created_at: "2024-04-08 14:42:36.121+00",
+    status: "complete",
+    is_public: true,
+    is_trending: false,
+    type: "concat",
+  },
+  {
     uuid: "ee9281e7-c0a6-4adf-b731-814169d1c33a",
     video_url: "https://cdn1.suno.ai/ee9281e7-c0a6-4adf-b731-814169d1c33a.mp4",
     audio_url: "https://cdn1.suno.ai/ee9281e7-c0a6-4adf-b731-814169d1c33a.mp3",
@@ -137,7 +408,7 @@ export async function getLatestSongs(
   const offset = (page - 1) * limit;
   // 延迟
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  const extendedSongs = [...songs];
+  const extendedSongs = songs.filter((song) => song.is_trending === true);
 
   return extendedSongs.slice(offset, offset + limit);
 }
